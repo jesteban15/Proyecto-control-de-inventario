ca5c7c7908b971a2821373747b7eb9b73cf8928b
Esteban <cardonaadrian081@gmail.com>
2026-03-26 17:23:20 -0500

    Aprendiendo customTkinter

--- interfazPractica.py
+++ interfazPractica.py
@@ -1,8 +1,38 @@
-from CTkTable import CTkTable
-import customtkinter
-
+# from CTkTable import CTkTable
+import customtkinter as tk
+"""
 root = customtkinter.CTk()
 value = [["Nombre", "Edad", "Ciudad"], [1, 2, 3], [4, 5, 6]]
 table = CTkTable(master=root, row=3, column=3, values=value)
 table.pack(expand=True, fill="both", padx=20, pady=20)
-root.mainloop()
+root.mainloop()"""
+
+class MyCheckboxFrame(customtkinter.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master)
+
+        self.checkbox_1 = customtkinter.CTkCheckBox(self, text="checkbox 1")
+        self.checkbox_1.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="w")
+        self.checkbox_2 = customtkinter.CTkCheckBox(self, text="checkbox 2")
+        self.checkbox_2.grid(row=1, column=0, padx=10, pady=(10, 0), sticky="w")
+
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("my app")
+        self.geometry("400x180")
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure(0, weight=1)
+
+        self.checkbox_frame = MyCheckboxFrame(self)
+        self.checkbox_frame.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="nsw")
+
+        self.button = customtkinter.CTkButton(self, text="my button", command=self.button_callback)
+        self.button.grid(row=3, column=0, padx=10, pady=10, sticky="ew")
+
+    def button_callback(self):
+        print("button pressed")
+
+app = App()
+app.mainloop()
