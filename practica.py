1f2efdeb6e1b5848717fe10e0e7f3c478a8c9635
Esteban <cardonaadrian081@gmail.com>
2026-03-30 17:36:44 -0500

    Agregando practica de uso de customTKinter

--- practica.py
+++ practica.py
@@ -1,26 +1,32 @@
 import customtkinter as tk
 
+class MyCheckboxFrame(tk.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master)
+
+        self.checkbox_1 = tk.CTkCheckBox(self, text="Checkbox 1")
+        self.checkbox_1.grid(row=0, column=0, padx=10, pady=(10,100), sticky="w")
+        self.checkbox_2 = tk.CTkCheckBox(self, text="Checkbox 2")
+        self.checkbox_2.grid(row=1, column=0, padx=10, pady=(10,0), sticky="w")
+        self.checkbox_3 = tk.CTkCheckBox(self, text="Checkbox 3")
+        self.checkbox_3.grid(row=2, column=0, padx=10, pady=(10, 0), sticky="w")
+
 class App(tk.CTk):
     def __init__(self):
         super().__init__()
 
-        self.title("my app")
+        self.title("My app")
         self.geometry("400x180")
         self.grid_columnconfigure(0, weight=1)
         self.grid_rowconfigure(0, weight=1)
 
-        self.checkbox_frame = tk.CTkFrame(self)
-        self.checkbox_frame.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="nsw")
-        self.checkbox_1 = tk.CTkCheckBox(self.checkbox_frame, text="checkbox 1")
-        self.checkbox_1.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="w")
-        self.checkbox_2 = tk.CTkCheckBox(self.checkbox_frame, text="checkbox 2")
-        self.checkbox_2.grid(row=1, column=0, padx=10, pady=(10, 0), sticky="w")
+        self.checkbox_frame = MyCheckboxFrame(self)
+        self.checkbox_frame.grid(row=0, column=0, padx=10, pady=(10,0), sticky="nsw")
 
-        self.button = tk.CTkButton(self, text="my button", command=self.button_callback)
-        self.button.grid(row=3, column=0, padx=10, pady=10, sticky="ew")
-
+        self.button = tk.CTkButton(self, text="My button", command=self.button_callback)
+        self.button.grid(row=2, column=0, padx=10, pady=10, sticky="ew")
     def button_callback(self):
-        print("button pressed")
+        print("Button pressed")
 
 app = App()
 app.mainloop()
